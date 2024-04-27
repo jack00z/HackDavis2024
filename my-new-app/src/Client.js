f4923fb4a367fa92d307b09a1144260fa1e4d74a
Jack Zheng <jackzheng274@gmail.com>
2024-04-27 16:44:49 -0700

    Beginning

---
+++ my-new-app/src/Client.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://zradvwvufhhhwtpeenhv.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpyYWR2d3Z1ZmhoaHd0cGVlbmh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQyNTIwNzksImV4cCI6MjAyOTgyODA3OX0.XPhuWlfsggO2vVvmhFNnZOZQ5KgYWBWGUpU9GdWQrUA"
+);
