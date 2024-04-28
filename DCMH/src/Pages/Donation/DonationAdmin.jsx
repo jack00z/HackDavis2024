d8aeaa03c8e99bfa09c246b85094404110bc70b8
calauim <callau@ucdavis.edu>
2024-04-27 23:47:48 -0700

    Sorting Buttons

--- DCMH/src/Pages/Donation/DonationAdmin.jsx
+++ DCMH/src/Pages/Donation/DonationAdmin.jsx
@@ -7,35 +7,28 @@ import CleaningCard from "../../Components/CleaningCard";
 const Donation = () => {
 
   const [cleaning, setCleaning] = useState([]);
-  const[order, setOrder] = useState('name')
 
   useEffect(() => {
     const getCleaning = async () => {
       const { data } = await supabase
         .from('cleaning')
-        .select()
-        .order(order)
+        .select();
 
         setCleaning(data);
     }
 
     getCleaning();
 
-  }, [order]);
+  }, []);
 
   return (
     <div>
       <h2>Donation Page</h2>
       {cleaning && (
         <div className="cleaning">
-          <div className="order">
-            <p>Sort by:</p>
-            <button onClick={() => setOrder('quantity')}>Quantity</button>
-            <button onClick={() => setOrder('name')}>Name</button>
             {cleaning.map((item) => (
               <CleaningCard key={item.id} cleaning={item} />
             ))}
-      </div>
       </div>
       )}
     </div>
