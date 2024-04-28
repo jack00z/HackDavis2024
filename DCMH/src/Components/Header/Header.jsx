5f567ffaea6288d13df8b297270497293c67ace3
Jack Zheng <jackzheng274@gmail.com>
2024-04-28 13:25:37 -0700

    Added unfinished skeleton of home page

--- DCMH/src/Components/Header/Header.jsx
+++ DCMH/src/Components/Header/Header.jsx
@@ -1,24 +1,29 @@
 import React from "react";
 import "./Header.css";
+//Navigate
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  let navigate = useNavigate();
+
+  const handleClick = (route) => {
+    navigate(route);
+  };
+
   return (
-    <header>
-      <h1>My Website</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <div className="h-container">
+      <div className="nav-container">
+        <div className="home-button">Home</div>
+        <uli>About Us</uli>
+        <uli>Programs</uli>
+        <uli>Events</uli>
+        <uli>Volunteer</uli>
+        <uli>Contact Us</uli>
+        <uli className="donate-button" onClick={() => handleClick("/donation")}>
+          Donate
+        </uli>
+      </div>
+    </div>
   );
 };
 
--- DCMH/src/Pages/Home/Home.jsx
+++ DCMH/src/Pages/Home/Home.jsx
@@ -1,10 +1,58 @@
 import React from "react";
 import "./Home.css";
 
+//Navigate
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
+  let navigate = useNavigate();
+  const handleClick = (route) => {
+    navigate(route);
+  };
+
   return (
-    <div>
-      <div className="UserName">HELLO</div>
+    <div className="parent">
+      <div className="tparent-container">
+        <div className="title-container">
+          <div className="title">Davis Community Meals and Housing</div>
+        </div>
+      </div>
+      <div className="text-container">
+        <div className="text">
+          Davis Community Meals and Housing is a non-profit organization
+          dedicated to providing low-income and unhoused individuals and
+          families with housing, food, and human services to help them rebuild
+          their lives. Join us in our mission to change lives and make
+          everlasting impacts.
+        </div>
+        <div className="dono-container">
+          <button
+            className="donation-button"
+            onClick={() => handleClick("/donation")}
+          >
+            Donate
+          </button>
+        </div>
+      </div>
+      <div className="stats-container">
+        <div className="stats-pic"></div>
+        <div className="stats-num">
+          <span>30,000</span>
+          <span>70</span>
+          <span>9,000</span>
+          <span>500</span>
+        </div>
+        <div className="stats-words">
+          <div className="meals">Meals Provided</div>
+          <div className="volunteers">Volunteers</div>
+          <div className="dollars">Dollars Raised</div>
+          <div className="housing">Housing Provided</div>
+        </div>
+      </div>
+      <div className="resource-container">
+        Our Resources
+        <div className="resouces"></div>
+      </div>
     </div>
   );
 };
